9590c1ffbf35b0b93466b35c78ffae5145aebcff
Lucas Vellin-Patché <lucvellin@gmail.com>
2025-04-09 15:20:10 +0200

    Trying interrups

--- Simon/src/main.cpp
+++ Simon/src/main.cpp
@@ -10,65 +10,84 @@ const int buttonR = 26;
 const int ledR =  16;
 const int buzzer = 32;
 
-
-int gButtonState = 0;
-int yButtonState = 0;
-int bButtonState = 0;
-int rButtonState = 0;
+char input = 'N';
+bool gState = false;
+bool yState = false;
+bool bState = false;
+bool rState = false;
 
 // put function declarations here:
 //int myFunction(int, int);
+void update();
+void IRAM_ATTR gTrigger();
+void IRAM_ATTR yTrigger();
+void IRAM_ATTR bTrigger();
+void IRAM_ATTR rTrigger();
 
 void setup() {
   Serial.begin(115200);
   pinMode(buttonG, INPUT_PULLUP);
+  attachInterrupt(buttonG, gTrigger, CHANGE);
   pinMode(ledG, OUTPUT);
   pinMode(buttonY, INPUT_PULLUP);
+  attachInterrupt(buttonY, yTrigger, CHANGE);
   pinMode(ledY, OUTPUT);
   pinMode(buttonB, INPUT_PULLUP);
+  attachInterrupt(buttonB, bTrigger, CHANGE);
   pinMode(ledB, OUTPUT);
   pinMode(buttonR, INPUT_PULLUP);
+  attachInterrupt(buttonR, rTrigger, CHANGE);
   pinMode(ledR, OUTPUT);
   pinMode(buzzer, OUTPUT);
 }
 
 void loop() {
-  gButtonState = digitalRead(buttonG);
-  Serial.println("green:" + gButtonState);
-  if (gButtonState == LOW) {
-      digitalWrite(ledG, HIGH);
-      tone(buzzer, 262, 10);
+  update();
+}
 
-  } else {
-      digitalWrite(ledG, LOW);
+void update(){
+  if (gState){
+    digitalWrite(ledG, HIGH);
+    tone(buzzer, 262, 10);
   }
-  yButtonState = digitalRead(buttonY);
-  Serial.println("yellow:" + yButtonState);
-  if (yButtonState == LOW) {
-      digitalWrite(ledY, HIGH);
-      tone(buzzer, 294, 10);
-  } else {
-      digitalWrite(ledY, LOW);
+  else{
+    digitalWrite(ledG, LOW);
   }
-  bButtonState = digitalRead(buttonB);
-  Serial.println("green:" + bButtonState);
-  if (bButtonState == LOW) {
-      digitalWrite(ledB, HIGH);
-      tone(buzzer, 330, 10);
-  } else {
-      digitalWrite(ledB, LOW);
+  if (yState){
+    digitalWrite(ledY, HIGH);
+    tone(buzzer, 294, 10);
   }
-  rButtonState = digitalRead(buttonR);
-  Serial.println("yellow:" + rButtonState);
-  if (rButtonState == LOW) {
-      digitalWrite(ledR, HIGH);
-      tone(buzzer, 349, 10);
-  } else {
-      digitalWrite(ledR, LOW);
+  else{
+    digitalWrite(ledY, LOW);
+  }
+  if (bState){
+    digitalWrite(ledB, HIGH);
+    tone(buzzer, 330, 10);
+  }
+  else{
+    digitalWrite(ledB, LOW);
+  }
+  if (rState){
+    digitalWrite(ledR, HIGH);
+    tone(buzzer, 349, 10);
+  }
+  else{
+    digitalWrite(ledR, LOW);
   }
 }
 
-// put function definitions here:
-//int myFunction(int x, int y) {
-//  return x + y;
-//}
+void IRAM_ATTR gTrigger() {
+  gState = !gState;
+}
+
+void IRAM_ATTR yTrigger() {
+  yState = !yState;
+}
+
+void IRAM_ATTR bTrigger() {
+  bState = !bState;
+}
+
+void IRAM_ATTR rTrigger() {
+  rState = !rState;
+}
